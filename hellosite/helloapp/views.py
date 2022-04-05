4134b48b4f3d1f775470ec0f2a052e214e2ff48b
Shawner1 <ShawnHing12@gmail.com>
2022-04-04 21:41:37 -0400

    create urls.py & add view

--- hellosite/helloapp/views.py
+++ hellosite/helloapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views import View
+
+# Create your views here.
+class HelloWorldView(View):
+    def get(self, request):
+        return render(request=request, template_name='hello_world.html')
